c736366a969e074313a03f2da4e8f6186e92de31
Seneng Utami <senengutami2@gmail.com>
2023-07-10 18:47:46 +0545

    created invoice app

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Center from "./components/Center";
+import Header from "./components/Header";
+import { AnimatePresence } from "framer-motion";
+import { Routes, Route, useLocation } from "react-router-dom";
+import InvoiceInfo from "./components/InvoiceInfo";
+import invoiceSlice from "./redux/invoiceSlice";
+import { useDispatch } from "react-redux";
+
+function App() {
+  const location = useLocation();
+  const dispatch = useDispatch();
+
+  const onDelete = (id) => {
+    dispatch(invoiceSlice.actions.deleteInvoice({ id: id }));
+  };
+
+  return (
+    <div className=" dark:bg-[#afe3fd] duration-300 min-h-screen bg-[#afe3fd]">
+      <Header />
+
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route element={<Center />} path="" />
+          <Route
+            element={<InvoiceInfo onDelete={onDelete} />}
+            path="/invoice"
+          />
+        </Routes>
+      </AnimatePresence>
+    </div>
+  );
+}
+
+export default App;
